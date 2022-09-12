194861bf959a1319e075dfc7390e31b0212a57db
Antony <a.miroshnikov@dunice.net>
2022-09-12 17:31:42 +0300

    news receiving across GET fixed mistakes

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,5 @@ use App\Http\Controllers\Posttag;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::resource('posts', PostController::class);
